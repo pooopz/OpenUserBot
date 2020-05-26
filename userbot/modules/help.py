5edac92cd76973b89f1a7c7e1f9ba3f937f341c4
pooopz <49567451+pooopz@users.noreply.github.com>
2020-05-26 16:07:50 +0530

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -17,12 +17,12 @@ async def help(event):
         if args in CMD_HELP:
             await event.edit(str(CMD_HELP[args]))
         else:
-            await event.edit("correct per paradaa...")
+            await event.edit("Please specify a valid module name.")
     else:
         await event.edit("Please specify which module do you want help for !!\
             \nUsage: .help <module name>")
         string = ""
         for i in CMD_HELP:
             string += "`" + str(i)
-            string += "`¥ "
+            string += "` 🙂 "
         await event.reply(string)
